fb3862f2e35c7c7aadb3ecf6cc478f1168f3d9ca
Thirki <henriquegomesdev@outlook.com>
2024-06-18 14:40:21 -0300

    chore: add styled components and configure theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes";
+import GlobalStyle from "./styles/globalStyles.ts";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider theme={defaultTheme}>
+        <App />
+        <GlobalStyle />
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body, html, * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    line-height: 140%;
+  }
+  body {
+  }
+`;
+
+export default GlobalStyle;
